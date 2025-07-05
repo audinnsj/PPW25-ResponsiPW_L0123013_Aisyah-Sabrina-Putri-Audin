d89de8d4bc0daf1877467edc4985c733b639c20e
audinnsj <aisyahsabrina235@gmail.com>
2025-07-06 00:42:13 +0700

    ubah inkonsisten yang ada

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Auth;
 
-// Tampilan utama (bisa diakses guest dan login)
-Route::get('/', [MenuController::class, 'index'])->name('menu.index');
+// Redirect root URL ke daftar menu
+Route::get('/', function () {
+    return redirect()->route('menus.index');
+});
 
-// Auth bawaan Laravel
+// Auth Laravel
 Auth::routes();
 
-// Semua fitur CRUD hanya untuk yang sudah login
+// Semua pengguna (guest & login) bisa lihat daftar menu
+Route::get('/menus', [MenuController::class, 'index'])->name('menus.index');
+
+// Fitur CRUD hanya untuk yang login
 Route::middleware(['auth'])->group(function () {
     Route::get('/menus/create', [MenuController::class, 'create'])->name('menus.create');
     Route::post('/menus', [MenuController::class, 'store'])->name('menus.store');
