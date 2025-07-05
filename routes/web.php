210ca87be521b4b3aae707da243c6f065a6899a3
audinnsj <aisyahsabrina235@gmail.com>
2025-07-05 23:08:22 +0700

    perbaiki routing halaman

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Tampilan utama (bisa diakses guest dan login)
+Route::get('/', [MenuController::class, 'index'])->name('menu.index');
 
+// Auth bawaan Laravel
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Semua fitur CRUD hanya untuk yang sudah login
+Route::middleware('auth')->group(function () {
+    Route::get('/menus/create', [MenuController::class, 'create'])->name('menus.create');
+    Route::post('/menus', [MenuController::class, 'store'])->name('menus.store');
+    Route::get('/menus/{menu}/edit', [MenuController::class, 'edit'])->name('menus.edit');
+    Route::put('/menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
+    Route::delete('/menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
+});
